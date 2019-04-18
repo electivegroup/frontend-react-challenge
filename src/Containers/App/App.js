5512ae399a3be23e03f1d5979e374343cf5287dc
Bernard Baker <bernard.baker.me@gmail.com>
2019-04-18 19:29:17 +0100

    Added custom card to UI.

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,6 +2,8 @@ import React, { Component, lazy, Suspense } from "react";
 import { getData } from "../../data/Store";
 import packageJson from "../../package.config.json";
 
+import CustomCard from "../Card/CustomCard";
+
 const Board = lazy(() => import("react-trello"));
 
 class App extends Component {
@@ -10,7 +12,9 @@ class App extends Component {
       <>
         <kbd>Version: {packageJson.version}</kbd>
         <Suspense fallback={<h1>Loading...</h1>}>
-          <Board data={getData()} draggable editable />
+          <Board data={getData()} draggable editable customCardLayout>
+            <CustomCard />
+          </Board>
         </Suspense>
       </>
     );
