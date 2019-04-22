e3d5f188fadc9a995225f3e70472986f735fd6c9
Bernard Baker <bernard.baker.me@gmail.com>
2019-04-22 10:46:40 +0100

    build(preinstall.js): At run time the package referenced isn't installed.
    https://github.com/bernardbaker/kanban/issues/13

--- preinstall.js
+++ preinstall.js
@@ -1,9 +1,10 @@
 const package = require("./package.json");
 const fs = require("fs");
-require("dotenv").config();
+const {
+  GITHUB_USER = "bernardbaker",
+  GITHUB_TOKEN = "ff5e35176173e66849728e7e834dc031dbf18bb3"
+} = process.env;
 
-const { GITHUB_USER, GITHUB_TOKEN } = process.env;
-//"react-trello": "https://bernardbaker:ff5e35176173e66849728e7e834dc031dbf18bb3@github.com/bernardbaker/electivegroup-react-trello.git#dev/02-enhance-project",
 package.dependencies["react-trello"] = package.dependencies["react-trello"]
   .replace("${GITHUB_USER}", GITHUB_USER)
   .replace("${GITHUB_TOKEN}", GITHUB_TOKEN);
